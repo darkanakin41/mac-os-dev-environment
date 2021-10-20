7238fb2cc88e29493895bd395947204ce42f8ec7
darkanakin41 <pierre.lejeune@groupeconseiletgestion.fr>
2021-10-20 09:44:30 +0200

    feat: add a dependencies installation

---
+++ setup/dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# installation of mutagen
+brew install mutagen-io/mutagen/mutagen
+
+# installation of yq
+brew install yq
+yq shell-completion bash > /usr/local/etc/bash_completion.d/yq
+
+# installation of watch
+brew install watch
+# installation of htop
+brew install watch
