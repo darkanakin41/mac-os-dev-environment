bb5d6ef9c482183edb7a84e43e9f1fbd3abc50c5
darkanakin41 <pierre.lejeune@groupeconseiletgestion.fr>
2021-11-23 09:58:55 +0100

    feat(config): Add Symfony Checks

--- toolbox/src/config.dist.js
+++ toolbox/src/config.dist.js
@@ -6,4 +6,12 @@ module.exports = {
 
     DOCKER_PHP_CONTAINER:'<you php container>',
     DOCKER_APACHE_CONTAINER:'<you apache container>',
+
+    SYMFONY_CHECKS: {
+        'doctrine/orm': ['php bin/console doctrine:schema:validate --skip-sync --no-interaction'],
+        'symfony/yaml': ['php bin/console lint:yaml config --parse-tags'],
+        'twig/twig': ['php bin/console lint:twig templates'],
+        'phpstan/phpstan': ['php vendor/bin/phpstan analyse src --level=5'],
+        'friendsofphp/php-cs-fixer': ['php vendor/bin/php-cs-fixer fix src --dry-run'],
+    }
 }
